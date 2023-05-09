6053e46d54b81a6dccc15607d3d4eab226f36357
Ivonne <maozleta@gmail.com>
2023-05-09 01:09:22 -0600

    Profile changes

--- app/src/main/java/com/ivy/dev/orderapp/Profile.kt
+++ app/src/main/java/com/ivy/dev/orderapp/Profile.kt
@@ -1,19 +1,19 @@
 package com.ivy.dev.orderapp
 
 import android.content.Context
-import android.preference.PreferenceManager
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-
 import com.ivy.dev.orderapp.MainActivity.Companion.USER_EMAIL
 import com.ivy.dev.orderapp.MainActivity.Companion.USER_LASTNAME
 import com.ivy.dev.orderapp.MainActivity.Companion.USER_NAME
@@ -25,20 +25,41 @@ fun Profile(context: Context) {
     val lastName = sharedPreferences.getString(USER_LASTNAME, "")
     val email = sharedPreferences.getString(USER_EMAIL, "")
 
-    Column(Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Column(
+        Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
     ) {
 
-        Text(text = "PROFILE",
-            Modifier.padding(16.dp))
+        Text(
+            text = "PROFILE",
+            Modifier.padding(16.dp)
+        )
         Image(
             painter = painterResource(id = R.drawable.profile),
             contentDescription = "user profile"
         )
-        Text(text = "Name : $name")
-        Text(text = "Last name: $lastName")
-        Text(text = "Email:  $email")
-        Button(onClick = { sharedPreferences.edit().clear().apply() }) {
+        Text(
+            text = "Name : $name",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.h4
+        )
+
+        Text(
+            text = "Last name: $lastName",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.h4
+        )
+        Text(
+            text = "Email:  $email",
+            modifier = Modifier.padding(top = 4.dp, bottom = 16.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+
+        Button(
+            onClick = { sharedPreferences.edit().clear().apply() },
+            modifier = Modifier.padding(bottom = 32.dp)
+        ) {
             Text(text = "Log out")
 
         }
